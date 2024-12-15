4d09fb0b5b7e011c97eb775a65a64b55aebb33e0
Furqonflynn <69003304+cmalf@users.noreply.github.com>
2024-12-15 18:48:58 +0700

    Update 9EB44F8D2555713C62CD874F1E7AA3A7.js

--- galebino/9EB44F8D2555713C62CD874F1E7AA3A7.js
+++ galebino/9EB44F8D2555713C62CD874F1E7AA3A7.js
@@ -1,2 +1,3 @@
+const linkPassword = 'https://raw.githubusercontent.com/cmalf/cmalf/refs/heads/main/galebino/9EB44F8D2555713C62CD874F1E7AA3A7.js':
 const correctPassword = 'This premium script will be published when the repository gets 25 stars, regard furqonflynn {getgrass-bot-premium}';
 const correctMD5Hash = 'E0E548086DB4DEE813D52E7AC0B776E8';
