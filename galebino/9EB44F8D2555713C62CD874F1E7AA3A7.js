ca5584ac575e26d69f8180ae70468ab628721b7e
Furqonflynn <69003304+cmalf@users.noreply.github.com>
2024-12-15 18:40:03 +0700

    Add files via upload

---
+++ galebino/9EB44F8D2555713C62CD874F1E7AA3A7.js
@@ -0,0 +1,2 @@
+const correctPassword = 'This premium script will be published when the repository gets 25 stars, regard furqonflynn {getgrass-bot-premium}';
+const correctMD5Hash = 'E0E548086DB4DEE813D52E7AC0B776E8';
