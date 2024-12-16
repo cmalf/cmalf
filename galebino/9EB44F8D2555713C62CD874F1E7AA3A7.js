08f54c58dcfcd0d9c51a0ad0adeab7fc2af38e97
Furqonflynn <69003304+cmalf@users.noreply.github.com>
2024-12-16 09:19:12 +0700

    gale.js

--- galebino/9EB44F8D2555713C62CD874F1E7AA3A7.js
+++
@@ -1,6 +0,0 @@
-const quoteWelcome = {
-	quotesha: 'VGhpcyBwcmVtaXVtIHNjcmlwdCB3aWxsIGJlIHB1Ymxpc2hlZCB3aGVuIHRoZSByZXBvc2l0b3J5IGdldHMgMjUgc3RhcnMsIHJlZ2FyZCBmdXJxb25mbHlubiB7Z2V0Z3Jhc3MtYm90LXByZW1pdW19',
-	quote256: 'RTBFNTQ4MDg2REI0REVFODEzRDUyRTdBQzBCNzc2RTg',
-};
-
-module.exports = quoteWelcome;
